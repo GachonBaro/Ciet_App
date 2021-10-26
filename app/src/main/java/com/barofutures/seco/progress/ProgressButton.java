6bcd2099776b782c2730fb4ce94bc45df0bee841
HyeonseoJUNG <windymay1920@gmail.com>
2021-10-27 01:58:25 +0900

    Add open source license notices

--- app/src/main/java/com/barofutures/seco/progress/ProgressButton.java
+++ app/src/main/java/com/barofutures/seco/progress/ProgressButton.java
@@ -1,3 +1,18 @@
+/*
+Copyright 2021 Baro Futures
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package com.barofutures.seco.progress;
 
 import android.content.Context;
@@ -15,8 +30,6 @@ public class ProgressButton {
     private ConstraintLayout layout;
     private ProgressBar progressBar;
     private TextView textView;
-
-    Animation fade_in;
 
     public ProgressButton(Context mContext, View view) {
         this.layout = view.findViewById(R.id.progress_button);
